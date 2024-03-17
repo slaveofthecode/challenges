fa87fe7f0ce679436de475e050cfc33e49ff905d
Gustavo Lopez <gustavo@getkalto.com>
2024-03-17 16:52:51 -0300

    Port was changed to 3000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const express = require("express");
 const mustacheExpress = require("mustache-express");
 const data = require("./data.json");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
