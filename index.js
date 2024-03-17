9e25b574d5ad574b34a6f85292afa549232dc796
Gustavo Lopez <gustavo@getkalto.com>
2024-03-17 15:52:39 -0300

    ...1

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log("Server is running on port 3000");
+	console.log(`Server is running on port ${port}`);
 });
 
 // export default app;
