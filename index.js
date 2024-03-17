3b268a4cfea926ffb187b268daf257b25961f2bb
Gustavo Lopez <gustavo@getkalto.com>
2024-03-17 17:04:19 -0300

    It changed to module type

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-// import express from "express";
-// import mustacheExpress from "mustache-express";
-// import path from "path";
-// import data from "./data.json" assert { type: "json" };
-
-const express = require("express");
-const mustacheExpress = require("mustache-express");
-const data = require("./data.json");
+import express from "express";
+import mustacheExpress from "mustache-express";
+import path from "path";
+import data from "./data.json" assert { type: "json" };
 
 const port = process.env.PORT || 3000;
 
@@ -13,18 +9,22 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
 
 // Set up the public directory
-app.use("/public", express.static("public"));
-app.use(express.static(__dirname + "/public"));
-// ---
+app.use(
+	"/public",
+	express.static(
+		path.join(path.dirname(new URL(import.meta.url).pathname), "public")
+	)
+);
 
 // Set up mustache as the view engine
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
-app.set("views", __dirname + "/views");
-// ---
+app.set(
+	"views",
+	path.join(path.dirname(new URL(import.meta.url).pathname), "views")
+);
 
 app.get("/", (req, res) => {
 	res.render("index", data);
@@ -34,5 +34,4 @@ app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
 
-// export default app;
-module.exports = app;
+export default app;
