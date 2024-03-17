8549bf81b6bd1ce0eb32257ee106dc313c3f660f
Gustavo Lopez <gustavo@getkalto.com>
2024-03-17 17:07:17 -0300

    Restored to __dirname

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import express from "express";
-import mustacheExpress from "mustache-express";
-import path from "path";
-import data from "./data.json" assert { type: "json" };
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const data = require("./data.json");
 
 const port = process.env.PORT || 3000;
 
@@ -11,20 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Set up the public directory
-app.use(
-	"/public",
-	express.static(
-		path.join(path.dirname(new URL(import.meta.url).pathname), "public")
-	)
-);
+app.use("/public", express.static("public"));
+app.use(express.static(__dirname + "/public"));
+// ---
 
 // Set up mustache as the view engine
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
-app.set(
-	"views",
-	path.join(path.dirname(new URL(import.meta.url).pathname), "views")
-);
+app.set("views", __dirname + "/views");
+// ---
 
 app.get("/", (req, res) => {
 	res.render("index", data);
@@ -34,4 +28,4 @@ app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
 
-export default app;
+module.exports = app;
